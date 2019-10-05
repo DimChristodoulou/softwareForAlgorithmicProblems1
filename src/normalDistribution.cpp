8c52a988a03d7b4be9eba16f1c8b3e318e1734b7
GeorgeTsiros <geotsiros@hotmail.com>
2019-10-05 16:00:25 +0300

    Make normal distribution an actual function

--- src/normalDistribution.cpp
+++ src/normalDistribution.cpp
@@ -1,27 +1,22 @@
-// normal_distribution
 #include <iostream>
 #include <string>
 #include <random>
 
-int main(int argc, char const *argv[])
+
+double* normalDistribution(int wanted, int margin)
 {
 
-  // const int nrolls=10000;  // number of experiments
-  int wanted = 10;    // maximum number of stars to distribute
   int i = 0;
-
   std::default_random_engine generator;
   std::normal_distribution<double> distribution(5.0,2.0);
-
-  double p[wanted];
+  static double p[sizeof(wanted) / 4];
 
   while ( i < wanted) {
     double number = distribution(generator);
-    if ((number>=0.0)&&(number<10.0)) {
-    	p[i] = number;
-    	i++;
-    } 
-    
+    if ((number >= 0.0) && (number < margin )) {
+        p[i] = number;
+        i++;
+    }
   }
 
   std::cout << "normal_distribution (5.0,2.0):" << std::endl;
@@ -30,5 +25,17 @@ int main(int argc, char const *argv[])
     std::cout << p[j] << std::endl;
   }
 
-  return 0;
+  return p;
 }
+
+
+int main(int argc, char const *argv[]){
+
+  double* result;
+  result = normalDistribution(10, 10);
+  std::cout << "Result: " << std::endl;
+ for (int j=0; j<10; ++j) {
+    std::cout << result[j] << std::endl;
+  }
+}
+
